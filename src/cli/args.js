e39c3a8249ac0de7f23b1582b3b9279e76c56b09
elenaf007 <96072892+elenaf007@users.noreply.github.com>
2024-01-29 17:57:31 -0600

    feat cli args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,32 @@
 const parseArgs = () => {
     // Write your code here 
+    const propsNames=['--propName value','--prop2Name value2'];
+    let answers=[] ; 
+    let i=0;
+    function asking(i){
+        process.stdout.write(propsNames[i]);
+    };
+    process.stdin.on('data', function(data){
+      
+            answers.push(data.toString());
+            if (answers.length<propsNames.length){
+                asking(answers.length);
+            }else{
+                process.exit();
+            }
+            
+          
+    })
+    
+        
+    
+
+
+    process.on('exit', function(){
+        process.stdout.write(`propName is ${answers[0]}, prop2Name is ${answers[1]}`)
+    });
+
+    asking(0);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -5,7 +5,7 @@ const parseEnv = () => {
     process.stdin.on('data', function(data){
         //process.stdout.write('give me two names!');
 
-        process.stdout.write(`RSS_name1=${data.toString().split(' ')[0]} , RSS_name2=${data.toString().split(' ')[1]}`)
+        process.stdout.write(`RSS_name1=${data.toString().split(' ')[0]}; RSS_name2=${data.toString().split(' ')[1]}`)
          process.exit();
        })
 };
