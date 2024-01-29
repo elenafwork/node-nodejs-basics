5e7518cb1ef2aac9cdc83c4a27c347d2df1adf79
elenaf007 <96072892+elenaf007@users.noreply.github.com>
2024-01-29 14:02:44 -0600

    feat: read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,10 @@
+import fs from 'fs';
 const read = async () => {
     // Write your code here 
+    fs.readFile('src/fs/files/fileToRead.txt', function (err, data){
+        if (err) throw 'FS operation failed';
+        console.log(data.toString('utf8'))
+    })
 };
 
 await read();
