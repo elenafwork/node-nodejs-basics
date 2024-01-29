65a3dee95d4aa10e570fa288117395f88bd4ce4a
elenaf007 <96072892+elenaf007@users.noreply.github.com>
2024-01-29 14:06:57 -0600

    feat: delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,10 @@
+import fs from 'fs';
 const remove = async () => {
     // Write your code here 
+    fs.unlink('src/fs/files/fileToRemove.txt', function (err){
+        if (err)  throw 'FS operation failed'  ;
+        console.log('file is deleted');
+  })
 };
 
 await remove();
