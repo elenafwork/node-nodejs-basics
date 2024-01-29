c5737da4b2a7990c34c4fb73db449b262a7bb164
elenaf007 <96072892+elenaf007@users.noreply.github.com>
2024-01-29 15:49:10 -0600

    feat: streams/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import fs from 'fs';
+import {stdout} from 'node:process';
 const read = async () => {
     // Write your code here 
+    var rs=fs.createReadStream('src/streams/files/fileToRead.txt');
+    rs.on('data', function(data){
+        console.log('open');
+        
+        process.stdout.write(data)
+    })
 };
 
 await read();
