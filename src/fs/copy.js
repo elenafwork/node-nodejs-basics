986f7543129ad14473fb0f31b29fa7a8809ae623
elenaf007 <96072892+elenaf007@users.noreply.github.com>
2024-01-29 14:16:23 -0600

    feat: copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,14 @@
+import fs from 'fs-extra';
+var source='files';
+var destination='files_copy';
 const copy = async () => {
     // Write your code here 
+   fs.copy('src/fs/files', 'src/fs/files_copy', function (err){
+    if (err) throw 'FS operation failed';
+    console.log('complete!');
+    
+    
+   })
 };
 
 await copy();
